8e4610292457cb12360725a8f95ff6f905acee26
Ryan W. Januardi <100107440+kohryan@users.noreply.github.com>
2022-02-28 10:33:25 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import MakeYourBusiness from '../components/Common/MakeYourBusiness'
+{/* import MakeYourBusiness from '../components/Common/MakeYourBusiness' */}
 import News from '../components/Common/News'
-import Testimonials from '../components/Common/Testimonials'
+{/* import Testimonials from '../components/Common/Testimonials' */}
 import About from '../components/Home/About'
 import MainBanner from '../components/Home/MainBanner'
-import Services from '../components/Home/Services'
-import WhatWeOffer from '../components/Home/WhatWeOffer'
+{/*  import Services from '../components/Home/Services' */}
+{/* import WhatWeOffer from '../components/Home/WhatWeOffer' */}
 import WhyChooseUs from '../components/Home/WhyChooseUs'
 import Footer from '../components/Layouts/Footer'
 import Navbar from '../components/Layouts/Navbar'
