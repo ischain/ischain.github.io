01df56a21eb8380bbe9a7a5e501a63cfd5b47df9
Ryan W. Januardi <100107440+kohryan@users.noreply.github.com>
2022-02-28 16:16:35 +0800

    Update TeamTwo.js

--- components/Common/TeamTwo.js
+++ components/Common/TeamTwo.js
@@ -7,45 +7,26 @@ class TeamTwo extends Component {
             <section className="team-area pt-100 pb-70">
                 <div className="container">
                     <div className="section-title">
-                        <span>Team</span>
-                        <h2>Meet Our Genius Team</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias eos ea obcaecati et similique incidunt animi, sed, quos officiis placeat. Id ipsa molestias.</p>
+                        <span>Tim ISCHAIN</span>
+                        <p>Struktur tim ISCHAIN terdiri dari Advisory Board yang berisikan Pakar Blockchain dan Ustadz Pembina serta Pengurus yang berisikan Praktisi, Peneliti, Edukator, Developer, dan sebagainya.</p>
                     </div>
 
                     <div className="row">
-                        <div className="col-lg-4 col-sm-6">
+                        <div className="col-lg-3 col-sm-6">
                             <div className="single-team active">
-                                <div className="team-single-img">
-                                    <img src="/images/team/team5.png" alt="Image" />
-
-                                    <div className="team-img">
-                                        <img src="/images/team/team-shape.png" alt="Image" />
-                                    </div>
-                                </div>
-
                                 <div className="team-content">
                                     <h3>Yhouga A. Moppratama</h3>
                                     <span>CEO</span>
 
                                     <ul>
                                         <li>
-                                            <a href="https://www.facebook.com/" target="_blank">
+                                            <a href="https://www.facebook.com/yhouga.ariesta" target="_blank">
                                                 <i className="bx bxl-facebook"></i>
                                             </a>
                                         </li>
                                         <li>
-                                            <a href="https://twitter.com/" target="_blank">
-                                                <i className="bx bxl-twitter"></i>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://www.linkedin.com/" target="_blank">
+                                            <a href="https://id.linkedin.com/in/yhouga-ariesta" target="_blank">
                                                 <i className="bx bxl-linkedin"></i>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://www.pinterest.com/" target="_blank">
-                                                <i className="bx bxl-pinterest-alt"></i>
                                             </a>
                                         </li>
                                     </ul>
@@ -53,16 +34,8 @@ class TeamTwo extends Component {
                             </div>
                         </div>
 
-                        <div className="col-lg-4 col-sm-6">
+                        <div className="col-lg-3 col-sm-6">
                             <div className="single-team active">
-                                <div className="team-single-img">
-                                    <img src="/images/team/team6.png" alt="Image" />
-
-                                    <div className="team-img">
-                                        <img src="/images/team/team-shape.png" alt="Image" />
-                                    </div>
-                                </div>
-
                                 <div className="team-content">
                                     <h3>Kene Alex Piter</h3>
                                     <span>Executive</span>
@@ -93,7 +66,7 @@ class TeamTwo extends Component {
                             </div>
                         </div>
 
-                        <div className="col-lg-4 col-sm-6 offset-sm-3 offset-lg-0">
+                        <div className="col-lg-3 col-sm-6 offset-sm-3 offset-lg-0">
                             <div className="single-team active">
                                 <div className="team-single-img">
                                     <img src="/images/team/team2.png" alt="Image" />
