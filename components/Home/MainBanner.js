1f64747c5c2e42b00ab541452616dea612c6e569
Ryan W. Januardi <100107440+kohryan@users.noreply.github.com>
2022-03-05 07:34:11 +0800

    Update MainBanner.js

--- components/Home/MainBanner.js
+++ components/Home/MainBanner.js
@@ -8,16 +8,11 @@ const MainBanner = () => {
                 <div className="row align-items-center">
                     <div className="col-lg-6">
                         <div className="banner-text">
-                            <h1>Stay Halal with ISCHAIN</h1>
-                            <p>ISCHAIN adalah komunitas Islami penggiat industri Web3 halal<br></br><br></br>
-                            Visi kami menjadi media edukasi dan penyedia solusi Halal Crypto, Teknologi Blockchain, dan Web3 terbesar di Indonesia</p>	
+                            <h1>ISCHAIN (Islamic Cryptocurrency & Blockchain) Community</h1>
+                            <p>ISCHAIN adalah komunitas Islami penggiat industri Web3 halal. Visi kami menjadi media edukasi dan penyedia solusi Halal Crypto, Teknologi Blockchain, dan Web3 terbesar di Indonesia</p>	
                             <div className="banner-btn">
-                                <a className="default-btn" href="/tentang-kami" target="_blank">
-                                     Tentang Kami
-                                </a>
-
                                 <a className="default-btn" href="https://t.me/ischainlounge" target="_blank">
-                                     <i class="bx bxl-telegram"></i>  Telegram ISCHAIN
+                                     <i class="bx bxl-telegram"></i>  Bergabung
                                 </a>
                             </div>
                         </div>
@@ -50,42 +45,42 @@ const MainBanner = () => {
                         <div className="col-lg-4 col-sm-6 p-0">
                             <div className="single-features">
                                 <i className="flaticon-artificial-intelligence"></i>
-                                <h3>Ischain Academy</h3>
+                                <h3>Edukasi</h3>
                                 <p>Memberikan edukasi menyeluruh terkait blockchain dan aset crypto baik dari sisi syariat, fikih, teknologi, implementasi, hingga manajemen risiko yang dapat memberi kemaslahatan umat secara umum.</p>
 
-                                <Link href="/coming-soon">
+                                {/* <Link href="/coming-soon">
                                     <a className="read-more-icon">
                                         <span className="flaticon-right-arrow"></span>
                                     </a>
-                                </Link>
+                                </Link> */}
                             </div>
                         </div>
 
                         <div className="col-lg-4 col-sm-6 p-0">
                             <div className="single-features">
                                 <i className="flaticon-engineer"></i>
-                                <h3>Ischain Solution</h3>
-                                <p>Menyediakan solusi blockchain dan implementasi teknologinya kepada bisnis & usaha umat agar dapat meningkatkan daya saing dengan memanfaatkan seluruh keunggulan blockchain yang sesuai prinsip syariat Islam.</p>
+                                <h3>Kolaborasi</h3>
+                                <p>Membangun kolaborasi bersama pakar blockchain, edukator crypto, dan asatidzah/ulama untuk membangun ekosistem halal crypto dan pengembangan teknologi blockchain yang dapat memberi kemaslahatan untuk umat secara umum.</p>
                                 
-                                <Link href="/coming-soon">
+                                {/* <Link href="/coming-soon">
                                     <a className="read-more-icon">
                                         <span className="flaticon-right-arrow"></span>
                                     </a>
-                                </Link>
+                                </Link> */}
                             </div>
                         </div>
 
                         <div className="col-lg-4 col-sm-6 offset-sm-3 offset-lg-0 p-0">
                             <div className="single-features">
                                 <i className="flaticon-success"></i>
-                                <h3>Ischain DAO</h3>
-                                <p>Menjaga nilai treasury agar tumbuh konsisten dengan pengelolaan yang otomatis dan terdesentralisasi sesuai dengan prinsip-prinsip muamalah syariah yang dapat memberi kemaslahatan umat secara umum</p>
+                                <h3>Solusi</h3>
+                                <p>Menyediakan solusi blockchain dan implementasi teknologinya kepada bisnis dan usaha umat agar dapat meningkatkan daya saing dengan memanfaatkan seluruh keunggulan blockchain yang sesuai dengan prinsip syariat Islam.</p>
                                 
-                                <Link href="/coming-soon">
+                                {/* <Link href="/coming-soon">
                                     <a className="read-more-icon">
                                         <span className="flaticon-right-arrow"></span>
                                     </a>
-                                </Link>
+                                </Link> */}
                             </div>
                         </div>
                     </div>
