9c18c3ed521a310ecc8dfc26e3b17c63d51c641b
Ryan W. Januardi <100107440+kohryan@users.noreply.github.com>
2022-03-05 08:33:05 +0800

    Update About.js

--- components/About/About.js
+++ components/About/About.js
@@ -1,22 +1,13 @@
-import Link from 'next/link';
 import React, { Component } from 'react';
+import Link from 'next/link';
 
-class Aboout extends Component {
-
+class About extends Component {
     render() {
         return (
-        <section className="about-area">
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-lg-6">
-                        <div className="about-img">
-                            <img src="/images/about-img.png" alt="Image" />
-                        </div>
-                    </div>
-
-                    <div className="col-lg-6">
-                        <div className="about-content">
-                            <span>Tentang Kami</span>
+            <section className="about-area pt-100">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6">
                             <span>Tentang Kami</span>
                             <h2>ISCHAIN (Islamic Cryptocurrency and Blockchain) Community </h2>
                             <p>adalah sebuah komunitas islami penggiat industri web3 halal, termasuk diantaranya aset crypto, teknologi blockchain, dan aplikasi desentral. ISCHAIN Community percaya bahwa industri web3 akan menjadi sebuah ekonomi baru yang revolusioner yang disebut sebagai ekonomi token (token economy), dan umat Islam harus menjadi yang terdepan dalam pengadopsiannya tanpa melanggar kaidah dan prinsip Islam.</p>
@@ -51,13 +42,10 @@ class Aboout extends Component {
                                         </li>
                                     </ul>
                                 </div>
-    
                             </div>
-    
                         </div>
                     </div>
                 </div>
-            </div>
             </section>
         );
     }
