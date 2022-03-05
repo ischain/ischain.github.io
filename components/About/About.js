83e142a3485ec318ef3d1aabd390dd5661ff9f80
Ryan W. Januardi <100107440+kohryan@users.noreply.github.com>
2022-03-05 08:36:31 +0800

    Update About.js

--- components/About/About.js
+++ components/About/About.js
@@ -4,16 +4,22 @@ import Link from 'next/link';
 class About extends Component {
     render() {
         return (
-            <section className="about-area pt-100">
+            <section className="about-area">
                 <div className="container">
                     <div className="row align-items-center">
                         <div className="col-lg-6">
-                            <span>Tentang Kami</span>
-                            <h2>ISCHAIN (Islamic Cryptocurrency and Blockchain) Community </h2>
-                            <p>adalah sebuah komunitas islami penggiat industri web3 halal, termasuk diantaranya aset crypto, teknologi blockchain, dan aplikasi desentral. ISCHAIN Community percaya bahwa industri web3 akan menjadi sebuah ekonomi baru yang revolusioner yang disebut sebagai ekonomi token (token economy), dan umat Islam harus menjadi yang terdepan dalam pengadopsiannya tanpa melanggar kaidah dan prinsip Islam.</p>
+                            <div className="about-img">
+                                <img src="/images/about-img.png" alt="Image" />
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="about-content">
+                                <span>Tentang Kami</span>
+                                  <h2>ISCHAIN (Islamic Cryptocurrency and Blockchain) Community </h2>
+                                     <p>adalah sebuah komunitas islami penggiat industri web3 halal, termasuk diantaranya aset crypto, teknologi blockchain, dan aplikasi desentral. ISCHAIN Community percaya bahwa industri web3 akan menjadi sebuah ekonomi baru yang revolusioner yang disebut sebagai ekonomi token (token economy), dan umat Islam harus menjadi yang terdepan dalam pengadopsiannya tanpa melanggar kaidah dan prinsip Islam.</p>
                             
-                            <div className="row">
-                                <div className="col-lg-6 col-sm-6">
+                                 <div className="row">
+                                    <div className="col-lg-6 col-sm-6">
                                     <ul>
                                         <li>
                                             <i className="flaticon-checked"></i>
@@ -28,9 +34,9 @@ class About extends Component {
                                             Berpegang Pada Syariat Islam
                                         </li>
                                     </ul>
-                                </div>
+                                    </div>
 
-                                <div className="col-lg-6 col-sm-6">
+                                    <div className="col-lg-6 col-sm-6">
                                     <ul>
                                         <li>
                                             <i className="flaticon-checked"></i>
@@ -41,8 +47,10 @@ class About extends Component {
                                             Beranggotakan Praktisi, Pakar, dan Asatidzah
                                         </li>
                                     </ul>
-                                </div>
+                                    </div>
+                               </div>
                             </div>
+        
                         </div>
                     </div>
                 </div>
