8eb2d67d10475bedd2a57739fa6754ee37ccf8ca
Ryan W. Januardi <100107440+kohryan@users.noreply.github.com>
2022-03-05 08:16:36 +0800

    Update About.js

--- components/Home/About.js
+++ components/Home/About.js
@@ -17,37 +17,12 @@ const About = () => {
                             <h2>ISCHAIN (Islamic Cryptocurrency and Blockchain) Community </h2>
                             <p>adalah sebuah komunitas islami penggiat industri web3 halal, termasuk diantaranya aset crypto, teknologi blockchain, dan aplikasi desentral. ISCHAIN Community percaya bahwa industri web3 akan menjadi sebuah ekonomi baru yang revolusioner yang disebut sebagai ekonomi token (token economy), dan umat Islam harus menjadi yang terdepan dalam pengadopsiannya tanpa melanggar kaidah dan prinsip Islam.</p>
                             
-                            <div className="row">
-                                <div className="col-lg-6 col-sm-6">
-                                    <ul>
-                                        <li>
-                                            <i className="flaticon-checked"></i>
-                                            Edukasi Halal Crypto Asset
-                                        </li>
-                                        <li>
-                                            <i className="flaticon-checked"></i>
-                                            Solusi Teknologi Blockchain
-                                        </li>
-                                        <li>
-                                            <i className="flaticon-checked"></i>
-                                            Berpegang Pada Syariat Islam
-                                        </li>
-                                    </ul>
-                                </div>
-
-                                <div className="col-lg-6 col-sm-6">
-                                    <ul>
-                                        <li>
-                                            <i className="flaticon-checked"></i>
-                                            Pengembangan Ekonomi Umat
-                                        </li>
-                                        <li>
-                                            <i className="flaticon-checked"></i>
-                                            Beranggotakan Praktisi, Pakar, dan Asatidzah
-                                        </li>
-                                    </ul>
-                                </div>
+                            <div className="banner-btn">
+                                <a className="default-btn" href="/tentang-kami" target="_blank">
+                                    Selengkapnya
+                                </a>
                             </div>
+    
                         </div>
                     </div>
                 </div>
