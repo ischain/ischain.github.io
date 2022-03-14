8c3335e5bdd01769a7825691472f6d9fa98ac86d
Ryan W. Januardi <100107440+kohryan@users.noreply.github.com>
2022-03-14 13:36:31 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -27,37 +27,7 @@ Router.events.on("routeChangeError", () => {
 });
 
 export default class MyApp extends App {
-  componentDidMount() {
-    let comment = document.createComment(`
 
-=========================================================
-* Notus NextJS - v1.1.0 based on Tailwind Starter Kit by Creative Tim
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/notus-nextjs
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/notus-nextjs/blob/main/LICENSE.md)
-
-* Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-`);
-    document.insertBefore(comment, document.documentElement);
-  }
-  static async getStaticProps({ Component, router, ctx }) {
-    let pageProps = {};
-
-    if (Component.getStaticProps) {
-      pageProps = await Component.getStaticProps(ctx);
-    }
-
-    return { pageProps };
-  }
   render() {
     const { Component, pageProps } = this.props;
 
@@ -71,7 +41,6 @@ export default class MyApp extends App {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <title>ISCHAIN - Islamic Cryptocurrency & Blockchain Community</title>
-          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
         </Head>
         <Layout>
           <Component {...pageProps} />
